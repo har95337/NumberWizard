1df7b34b86986ae3e8213a9ad020446a1296c047
har95337 <hsingh18@ucsc.edu>
2016-10-30 13:18:48 -0700

    Completed Number Wizard
    Followed a Unity tutorial to create this game.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string name){
+		Debug.Log ("Level Load Requested for: " + name);
+		Application.LoadLevel(name);
+	}
+public void QuitRequest(){
+	Debug.Log ("I wanna quit!");	
+	Application.Quit();
+	}
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	
+	int maxGuessAllowed = 15;
+			public Text text;
+	
+	void Start () {
+			StartGame ();
+	}
+		
+	public void GuessHigher(){
+			min = guess;
+			NextGuess();
+	}
+	
+	public void GuessLower(){
+			max = guess;
+			NextGuess ();
+	}
+	
+	void StartGame(){
+			max = 1000;
+			min = 1;
+			NextGuess ();
+	}
+	
+	void NextGuess(){
+			guess = Random.Range (min, max + 1);
+			text.text = guess.ToString ();
+			maxGuessAllowed--;
+			if(maxGuessAllowed <= 0){
+			Application.LoadLevel ("Win");
+		}
+	}
+}
